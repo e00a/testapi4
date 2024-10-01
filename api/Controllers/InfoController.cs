86b337659b4ef4f454bdce46f76de9d81c1f03c4
0109-03 <0109-03@0109-03>
2024-10-01 15:56:31 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ api/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("info")]
+        public IActionResult Info()
+        {
+            return Ok("information");
+        }
+    }
+}
